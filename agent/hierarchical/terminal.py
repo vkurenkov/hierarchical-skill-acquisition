[agents] WIP for training the terminal policy for hierarchical agent 	- Fix random seeds -> Reproducible now 	- Add more logging and debugging information 	- Fix value returns calculation 	- Introduce epsilon-greedy exploration 	- Move experience replay to a separate module

agent/hierarchical/terminal.py
@@ -37,7 +37,7 @@ class TerminalPolicy(nn.Module):
         output:
             state_returns - a tensor of size (batch_size, 1)
         '''
-        return self.value_function(time_encoded)
+        return F.sigmoid(self.value_function(time_encoded))
     
     def forward(self, frames, instructions):
         '''
@@ -66,7 +66,10 @@ class TerminalPolicy(nn.Module):
             returns - a tensor of size (batch_size, 1)
             action_probs - a tensor of size (batch_size, num_actions)
         output:
-            loss - calculated loss for the minibatch
+            total_loss
+            value_loss
+            a2c_loss
+            entropy - how uncertain we are about what to choose
         '''
         vision_encoding = self.vision.forward(frames)
         language_encoding = self.language.forward(instructions)
@@ -78,14 +81,18 @@ class TerminalPolicy(nn.Module):
         importance_weight = (cur_action_probs / action_probs).gather(1, actions)
         cur_action_log_probs = cur_action_probs.log().gather(1, actions)
 
-        loss = -(importance_weight * (cur_action_log_probs * advantage)).mean()
+        # Compute losses: for value function and for policy
+        value_loss = (advantage**2).mean().sqrt()
+        a2c_loss = -(importance_weight * (cur_action_log_probs * advantage)).mean()
+        entropy = (-cur_action_probs * cur_action_probs.log()).sum(1).mean()
+        total_loss = a2c_loss + value_loss
 
         self.optimizer.zero_grad()
-        loss.backward(retain_graph=True)
-        nn.utils.clip_grad_norm(self.parameters(), 1.0)
+        total_loss.backward(retain_graph=True)
+        nn.utils.clip_grad_norm(self.parameters(), 5.0)
         self.optimizer.step()
 
-        return loss
+        return total_loss, value_loss, a2c_loss, entropy
 
 if __name__ == "__main__":
     # TESTING

train_terminal.py
@@ -11,6 +11,7 @@ from collections import namedtuple
 from torch.autograd import Variable
 from torch.distributions import Categorical
 from agent.hierarchical.terminal import TerminalPolicy
+from experience import ExperienceReplay, Memento
 from utils.malmo import wait_for_frames, wait_for_observations, preprocess_frame
 
 # Initialize agent host
@@ -26,35 +27,51 @@ logger = open(os.path.join(".", "logs", "training_policy_k=0.log"), "a")
 TASKS = [("Find", "Red"), ("Find", "Yellow"), ("Find", "Blue"), 
          ("Find", "Green"), ("Find", "White"), ("Find", "Black")]
 BATCH_SIZE = 32
-NUM_LOW_LEVEL_ACTIONS = 8
+SEED = 0
+NUM_LOW_LEVEL_ACTIONS = 6
 NUM_PAST_TIMESTEPS = 4
-max_EPISODE_LENGTH = 50
+max_EPISODE_LENGTH = 35
 DISCOUNT = 0.95
+REPLAY_SIZE = 700
+DESCRIPTION = "Sparse reward; Ordinary experience replay; Remove entropy from total loss;"
+EPSILON_START = 0.9
+EPSILON_DECAY = 0.00005 # Reach minimum in about 20k episodes
+EPSILON_MINIMUM = 0.05
 REWARD_THERSHOLD = 0.9
 VOCABULARY = {"Find": 0, "Red": 1, "Yellow": 2, "Blue": 3, "Green": 4, "White": 5, "Black": 6}
 
 # Initialize terminal policy
+torch.manual_seed(SEED)
 policy = TerminalPolicy(num_actions=NUM_LOW_LEVEL_ACTIONS, 
                         num_timesteps=NUM_PAST_TIMESTEPS,
                         vocabulary_size=len(VOCABULARY))
 
 # Write information about current training session
 logger.write("\nTraining Session at " + str(datetime.datetime.now()))
-logger.write("\nEpisode length: " + str(max_EPISODE_LENGTH) + "; Batch size: " + str(BATCH_SIZE))
-logger.write("\nNum low-level actions: " + str(NUM_LOW_LEVEL_ACTIONS) + "; Num memory timesteps: " + str(NUM_PAST_TIMESTEPS))
+logger.write("\nDescription: " + DESCRIPTION)
 logger.write("\nTasks to learn: ")
 for task in TASKS:
     logger.write(task[0] + " " + task[1] + "; ")
 logger.write("\n")
+logger.write("\nEpisode length: " + str(max_EPISODE_LENGTH) + "; Batch size: " + str(BATCH_SIZE))
+logger.write("\nNum low-level actions: " + str(NUM_LOW_LEVEL_ACTIONS) + "; Num memory timesteps: " + str(NUM_PAST_TIMESTEPS))
+logger.write("\nReplay size: " + str(REPLAY_SIZE) + "; Return Discount: " + str(DISCOUNT))
+logger.write("\nEpsilon Start: " + str(EPSILON_START) + "; Epsilon Decay: " + str(EPSILON_DECAY) + "; Epsilon Min: " + str(EPSILON_MINIMUM))
+logger.write("\nSeed: " + str(SEED))
 
 
 for task in TASKS:
     logger.write("\n---> Training for subtask: " + str(task[0]) + " " + str(task[1]))
 
-    Memento = namedtuple("Memento", "frames instruction action value_return action_probs")
-    replay_memory = []
+    replay_memory = ExperienceReplay(capaciy=REPLAY_SIZE)
     last_200_rewards = []
+    last_200_timesteps = []
+    last_200_entropy = []
     episode_num = 0
+    epsilon = EPSILON_START
+
+    # Define instruction early
+    instruction = torch.LongTensor([VOCABULARY[task[0]], VOCABULARY[task[1]]]).unsqueeze(0)
 
     while True:
         episode_num += 1
@@ -65,7 +82,8 @@ for task in TASKS:
         logger.flush()
 
         # Load training environment
-        my_mission = env.create_environment(task, seed=episode_num)
+        my_mission = env.create_environment(task, seed=SEED+episode_num)
+        random.seed(SEED + episode_num)
 
         # Start the environment
         max_retries = 3
@@ -98,9 +116,6 @@ for task in TASKS:
         print("Waiting for the first frame...")
         world_state, cur_frames, _ = wait_for_frames(agent_host)
 
-        # Define instruction early
-        instruction = Variable(torch.LongTensor([VOCABULARY[task[0]], VOCABULARY[task[1]]]).unsqueeze(0))
-
         # Now the episode really starts
         print("The mission has started...")
 
@@ -120,29 +135,41 @@ for task in TASKS:
             last_frames = preprocess_frame(memory_frames[0]).unsqueeze(0)
             for frame in memory_frames[1:]:
                 last_frames = torch.cat((last_frames, preprocess_frame(frame).unsqueeze(0)), 0)
-            action_probs = policy.forward(Variable(last_frames.unsqueeze(0)), instruction)
+
+            action_probs = policy.forward(Variable(last_frames.unsqueeze(0)), Variable(instruction))
+            top_action = torch.max(action_probs, 1)[1]
+            prob_choose_top = 1.0 - epsilon
+            action_probs[0, :] = action_probs[0, :] * (1 - prob_choose_top)
+            action_probs[0, top_action.data[0]] = action_probs[0, top_action.data[0]] + prob_choose_top
             sampled_action = Categorical(action_probs).sample()
+
+            print(action_probs)
+            print(epsilon)
 
             # Act and update the current observation
             cur_observation, cur_frame, done = env.act(agent_host, sampled_action.data[0])  
             num_timesteps += 1
 
+            if(done):
+                break
+
             # Calculate the reward based on the current task
             reward = env.get_reward(task, cur_observation, prev_observation)
-
-            episode_trajectory.append(Point(Variable(last_frames), instruction, sampled_action, 
-                                            reward, action_probs))
 
             # Check for episode's termination
             if(reward > 0):
                 done = True
             if(num_timesteps >= max_EPISODE_LENGTH):
                 done = True
+            
+            episode_trajectory.append(Point(last_frames.numpy(), instruction.numpy(), sampled_action.data.numpy(), reward, action_probs.data.numpy()))
 
             if(done):
                 break
 
         print("The mission has ended.")
+        epsilon -= EPSILON_DECAY
+        epsilon = max(EPSILON_MINIMUM, epsilon)
         logger.write("End at: " + str(datetime.datetime.now()) + "; ")
         logger.write("Reward: " + str(reward) + "; Num Timesteps: " + str(num_timesteps) + "; ")
 
@@ -155,11 +182,19 @@ for task in TASKS:
 
         # Check if mean reward is higher than we wanted it to be
         last_200_rewards.append(reward)
+        last_200_timesteps.append(num_timesteps)
         if(episode_num % 200 == 0):
             mean_reward = np.mean(last_200_rewards)
-            print("Mean reward for last 200 episodes: " + str(mean_reward))
+            mean_timesteps = np.mean(last_200_timesteps)
+            mean_entropy = np.mean(last_200_entropy)
             logger.write("\n---------> Mean Reward: " + str(mean_reward))
+            logger.write("; Mean Timesteps: " + str(mean_timesteps))
+            logger.write("; Mean Entropy: " + str(mean_entropy))
             last_200_rewards = []
+            last_200_timesteps = []
+            last_200_entropy = []
+            print("Save the model...")
+            torch.save(policy, os.path.join("checkpoints", "terminal-3_1-" + task[0] + "_" + task[1] + "-" + str(episode_num) + ".pt"))
 
             if(mean_reward >= REWARD_THERSHOLD):
                 logger.write("\n---------> Mean Reward achieved required threshold. Success!")
@@ -168,34 +203,34 @@ for task in TASKS:
         # Calculate episode returns and put the episode trajectory into the replay memory
         value_return = 0.0
         for point in reversed(episode_trajectory):
-            value_return += point.reward + DISCOUNT * value_return
+            value_return = point.reward + DISCOUNT * value_return
             replay_memory.append(Memento(point.frames, point.instruction,
-                                         point.action, Variable(torch.FloatTensor([value_return])), 
+                                         point.action, value_return, 
                                          point.action_probs))
         
         # Train the model
-        if(BATCH_SIZE <= len(replay_memory)):
-            mementos = random.sample(replay_memory, BATCH_SIZE)
-        else:
-            mementos = random.sample(replay_memory, len(replay_memory))
-        mementos_frames = mementos[0].frames.unsqueeze(0)
-        mementos_instructions = mementos[0].instruction
-        mementos_actions = mementos[0].action.unsqueeze(0)
-        mementos_returns = mementos[0].value_return.unsqueeze(0)
-        mementos_probs = mementos[0].action_probs
+        mementos = replay_memory.sample(BATCH_SIZE)
+        mementos_frames = Variable(torch.FloatTensor(mementos[0].frames).unsqueeze(0))
+        mementos_instructions = Variable(torch.LongTensor(mementos[0].instruction))
+        mementos_actions = Variable(torch.LongTensor(mementos[0].action).unsqueeze(0))
+        mementos_returns = Variable(torch.FloatTensor([mementos[0].value_return]).unsqueeze(0))
+        mementos_probs = Variable(torch.FloatTensor(mementos[0].action_probs))
         for memento in mementos[1:]:
-            mementos_frames = torch.cat((mementos_frames, memento.frames.unsqueeze(0)), 0)
-            mementos_instructions = torch.cat((mementos_instructions, memento.instruction), 0)
-            mementos_actions = torch.cat((mementos_actions, memento.action.unsqueeze(0)), 0)
-            mementos_returns = torch.cat((mementos_returns, memento.value_return.unsqueeze(0)), 0)
-            mementos_probs = torch.cat((mementos_probs, memento.action_probs), 0)
+            mementos_frames = torch.cat((mementos_frames, Variable(torch.FloatTensor(memento.frames).unsqueeze(0))), 0)
+            mementos_instructions = torch.cat((mementos_instructions, Variable(torch.LongTensor(memento.instruction))), 0)
+            mementos_actions = torch.cat((mementos_actions, Variable(torch.LongTensor(memento.action).unsqueeze(0))), 0)
+            mementos_returns = torch.cat((mementos_returns, Variable(torch.FloatTensor([memento.value_return]).unsqueeze(0))), 0)
+            mementos_probs = torch.cat((mementos_probs, Variable(torch.FloatTensor(memento.action_probs))), 0)
 
-        loss = policy.train(mementos_frames, mementos_instructions, 
-                            mementos_actions, mementos_returns, mementos_probs)
-        # Report the current loss
-        print("Current loss: " + str(loss.data[0]))
-        logger.write("Loss: " + str(loss.data[0]))
+        for i in range(random.randint(1, 4)):
+            total_loss, value_loss, a2c_loss, entropy = policy.train(mementos_frames, mementos_instructions, 
+                                mementos_actions, mementos_returns, mementos_probs)
 
-        # Checkpoint the model every 100 or 200 episodes
+        # Report current losses
+        last_200_entropy.append(entropy.data[0])
+        logger.write("; Entropy: " + str(entropy.data[0]))
+        logger.write("; Total Loss: " + str(total_loss.data[0]))
+        logger.write("; Value Loss: " + str(value_loss.data[0]))
+        logger.write("; A2C Loss: " + str(a2c_loss.data[0]))
 
 logger.close()